5972440158e723a73e9fbdcda52157b784b19345
MarkusLYZ <markoos203@gmail.com>
2024-09-11 01:19:59 +0800

    Trending

---
+++ components/SearchInput.jsx
@@ -0,0 +1,38 @@
+import { View, Text, TextInput, Image } from "react-native";
+import React from "react";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+import { icons } from "../constants";
+
+const SearchInput = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className="border-2 border-black-200 rounded-2xl focus:border-secondary 2-full h-16 px-4 bg-black-100 items-center flex-row space-x-4">
+      <TextInput
+        className="text-base mt-0.5 text-white flex-1 font-pregular"
+        value={value}
+        placeholder="Search for a video topic"
+        placeholderTextColor="#7b7b8b"
+        onChangeText={handleChangeText}
+        secureTextEntry={title === "Password" && !showPassword}
+      />
+      <TouchableOpacity>
+        <Image
+          source={icons.search}
+          className="w-5 h-5 "
+          resizeMode="contain"
+        ></Image>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SearchInput;
